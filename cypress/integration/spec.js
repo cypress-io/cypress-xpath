cf7ba9b82bf6fec8600bca9ab62adb8f69a1f14e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-05 16:38:14 -0500

    feat: wrap returned elements in jQuery, close #2

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -5,13 +5,35 @@ describe('cypress-xpath', () => {
     expect(cy).property('xpath').to.be.a('function')
   })
 
-  it('finds h1', () => {
-    cy.visit('cypress/integration/index.html')
-    cy.xpath('//h1').should('have.length', 1)
-  })
+  context('elements', () => {
+    beforeEach(() => {
+      cy.visit('cypress/integration/index.html')
+    })
 
-  it('gets h1 text', () => {
-    cy.visit('cypress/integration/index.html')
-    cy.xpath('//h1/text()').its('0.textContent').should('equal', 'cypress-xpath')
+    it('finds h1', () => {
+      cy.xpath('//h1').should('have.length', 1)
+      .then((el$) => {
+        expect(el$).to.have.property('jquery')
+      })
+    })
+
+    it('gets h1 text', () => {
+      cy.xpath('//h1/text()').its('0.textContent').should('equal', 'cypress-xpath')
+    })
+
+    describe('primitives', () => {
+      it('counts h1 elements', () => {
+        cy.xpath('count(//h1)').should('equal', 1)
+      })
+
+      it('returns h1 text content', () => {
+        cy.xpath('string(//h1)').should('equal', 'cypress-xpath')
+      })
+
+      it('returns boolean', () => {
+        cy.xpath('boolean(//h1)').should('be.true')
+        cy.xpath('boolean(//h2)').should('be.false')
+      })
+    })
   })
 })
